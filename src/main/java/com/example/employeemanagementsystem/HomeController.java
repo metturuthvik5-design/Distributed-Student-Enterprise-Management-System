e6eeca388d2db609d7de8a023a88fb51da72e8f2
Ruthvik <metturuthvik2005@gmail.com>
2026-05-10 05:27:13 +0530

    Completed Employee Management System Full Stack Project

--- src/main/java/com/example/employeemanagementsystem/HomeController.java
+++ src/main/java/com/example/employeemanagementsystem/HomeController.java
@@ -2,12 +2,13 @@ package com.example.employeemanagementsystem;
 
 import com.example.employeemanagementsystem.entity.Employee;
 import com.example.employeemanagementsystem.repository.EmployeeRepository;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
-@RestController
+@Controller
 public class HomeController {
 
     private final EmployeeRepository employeeRepository;
@@ -16,35 +17,45 @@ public class HomeController {
         this.employeeRepository = employeeRepository;
     }
 
-    @GetMapping("/")
-    public String home() {
-        return "Employee Management System Running Successfully!";
+    // HOME PAGE
+    @GetMapping("/home")
+    public String homePage() {
+        return "index";
     }
 
     // GET ALL EMPLOYEES
+    @ResponseBody
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
+
         return employeeRepository.findAll();
     }
 
+    // GET EMPLOYEE BY ID
+    @ResponseBody
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee Not Found"));
+    }
+
     // ADD EMPLOYEE
+    @ResponseBody
     @PostMapping("/employees")
     public Employee addEmployee(@RequestBody Employee employee) {
+
         return employeeRepository.save(employee);
     }
 
-    // GET EMPLOYEE BY ID
-    @GetMapping("/employees/{id}")
-    public Optional<Employee> getEmployeeById(@PathVariable Long id) {
-        return employeeRepository.findById(id);
-    }
-
     // UPDATE EMPLOYEE
+    @ResponseBody
     @PutMapping("/employees/{id}")
     public Employee updateEmployee(@PathVariable Long id,
                                    @RequestBody Employee updatedEmployee) {
 
-        Employee employee = employeeRepository.findById(id).orElseThrow();
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee Not Found"));
 
         employee.setName(updatedEmployee.getName());
         employee.setDepartment(updatedEmployee.getDepartment());
@@ -54,6 +65,7 @@ public class HomeController {
     }
 
     // DELETE EMPLOYEE
+    @ResponseBody
     @DeleteMapping("/employees/{id}")
     public String deleteEmployee(@PathVariable Long id) {
 
